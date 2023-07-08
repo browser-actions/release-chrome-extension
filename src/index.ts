ee48a09df09986d60a3cc865e3bc978fbdb8218c
Shin'ya Ueoka <ueokande@i-beam.org>
2023-07-08 05:21:11 +0000

    Implement main

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,39 @@
+import { CWSClient } from "./cws";
+import fs from "fs";
 import core from "@actions/core";
 
 async function run(): Promise<void> {
-  core.debug("Hello World!");
+  const clientId = core.getInput("oauth-client-id");
+  const clientSecret = core.getInput("oauth-client-secret");
+  const refreshToken = core.getInput("oauth-refresh-token");
+  const extensionId = core.getInput("extension-id");
+  const extensionPath = core.getInput("extension-path");
+
+  const c = new CWSClient({ clientId, clientSecret, refreshToken });
+
+  const zip = fs.createReadStream(extensionPath);
+  const uploadResult = await c.updateItem(extensionId, zip);
+  if (
+    uploadResult.uploadState !== "SUCCESS" &&
+    uploadResult.uploadState !== "IN_PROGRESS"
+  ) {
+    throw new Error(
+      `Failed to upload: ${uploadResult.uploadState} ${JSON.stringify(
+        uploadResult.itemError
+      )}`
+    );
+  }
+
+  core.info(`Uploaded extension: ${uploadResult.id}`);
+
+  const publishResult = await c.publishItem(extensionId);
+  console.log(`Published with result: ${publishResult.status.join(", ")}`);
 }
 
-run();
+(async () => {
+  try {
+    await run();
+  } catch (error) {
+    core.setFailed(String(error));
+  }
+})();
