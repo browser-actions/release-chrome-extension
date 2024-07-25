browser-actions/release-chrome-extension

commit 69c95245a1f86471f106e4bec3da6ec24d173d93

feat: bump runtime node version (#9)
Bump runtime version to node 20 and use native
[fetch()](https://nodejs.org/dist/latest-v20.x/docs/api/globals.html#fetch)
in Node.js.

src/cws.ts
@@ -1,7 +1,5 @@
 import type { ReadStream } from "node:fs";
 import { google } from "googleapis";
-import fetch from "node-fetch";
-import type { BodyInit } from "node-fetch";
 
 type UploadState = "FAILURE" | "IN_PROGRESS" | "NOT_FOUND" | "SUCCESS";
 
@@ -82,7 +80,7 @@ export class CWSClient {
   private async proceed<T>(
     method: string,
     path: string,
-    body?: BodyInit,
+    body?: ReadStream,
   ): Promise<T> {
     await this.getAccessToken();
 

action.yml
@@ -20,5 +20,5 @@ inputs:
     required: true
 
 runs:
-  using: 'node16'
+  using: 'node20'
   main: 'index.js'

commit a5f44d9602806a339b2e375e32515b51580eebb9

fix: fix request error (#13)
Fix an issue caused by
https://github.com/browser-actions/release-chrome-extension/pull/9. The
body in the native `fetch()` should be Blob instead of Node API in
Node.js. This PR fix the request body issue.

src/cws.ts
@@ -1,4 +1,3 @@
-import type { ReadStream } from "node:fs";
 import { google } from "googleapis";
 
 type UploadState = "FAILURE" | "IN_PROGRESS" | "NOT_FOUND" | "SUCCESS";
@@ -62,7 +61,7 @@ export class CWSClient {
     );
   }
 
-  async updateItem(extensionId: string, zip: ReadStream): Promise<ItemReponse> {
+  async updateItem(extensionId: string, zip: Blob): Promise<ItemReponse> {
     return this.proceed<ItemReponse>(
       "PUT",
       `/upload/chromewebstore/v1.1/items/${extensionId}`,
@@ -80,7 +79,7 @@ export class CWSClient {
   private async proceed<T>(
     method: string,
     path: string,
-    body?: ReadStream,
+    body?: Blob,
   ): Promise<T> {
     await this.getAccessToken();
 

src/index.ts
@@ -11,7 +11,7 @@ async function run(): Promise<void> {
 
   const c = new CWSClient({ clientId, clientSecret, refreshToken });
 
-  const zip = fs.createReadStream(extensionPath);
+  const zip = await fs.openAsBlob(extensionPath);
   const uploadResult = await c.updateItem(extensionId, zip);
   if (
     uploadResult.uploadState !== "SUCCESS" &&